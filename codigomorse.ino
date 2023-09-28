d1be5b9560b972903bb9c01cf4a9543769ff114c
martinasl1 <144990963+martinasl1@users.noreply.github.com>
2023-09-28 08:18:13 +0200

    Add files via upload

---
+++ codigomorse.ino
@@ -0,0 +1,31 @@
+int doDuration= 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // M 
+ 
+  // A
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                      // wait for a second
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
+
+  //R
+
+  //T
+  
+  //I
+
+  //N
+
+  //A
+
+}
