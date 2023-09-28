4e515184cc941ca67f28b2ba61a5c6df76cd3369
martinasl1 <144990963+martinasl1@users.noreply.github.com>
2023-09-28 08:39:49 +0200

    Add files via upload

--- codigomorse.ino
+++ codigomorse.ino
@@ -7,7 +7,16 @@ pinMode(LED_BUILTIN, OUTPUT);
 
 void loop() {
   // M 
- 
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
+ delay(dotduration);                      // wait for a second
+digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
+
   // A
  digitalWrite(LED_BUILTIN, HIGH); 
  delay(dotduration); 
@@ -19,13 +28,48 @@ void loop() {
  delay(dotduration*3); 
 
   //R
+igitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                       // wait for a second
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
+ delay(dotduration);                      // wait for a second
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                   
 
   //T
-  
+  digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
+ delay(dotduration);
   //I
-
+digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                      // wait for a second
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                     
   //N
+digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                      // wait for a second
 
   //A
-
+digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration); 
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration);                      // wait for a second
+ digitalWrite(LED_BUILTIN, HIGH); 
+ delay(dotduration*3);
+ digitalWrite(LED_BUILTIN,LOW); 
+ delay(dotduration*3); 
 }
